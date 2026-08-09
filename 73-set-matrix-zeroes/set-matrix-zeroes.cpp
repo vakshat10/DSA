2d75cffb8880463a1a78a09e62d4a2b3c9bfcd51
vakshat10 <vakshatsw@gmail.com>
2026-08-09 23:15:31 +0530

    Time: 0 ms (100.00%) | Memory: 21 MB (33.94%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/set-matrix-zeroes.cpp
@@ -2,57 +2,52 @@ class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
 
-        int n = matrix.size();       // Number of rows
-        int m = matrix[0].size();    // Number of columns
+        int n = matrix.size();
+        int m = matrix[0].size();
 
-        // col0 tells whether the FIRST COLUMN should become zero
+        // int col[m] = {0};  ->matrix[0][...]
+        // int row[n] = {0};  ->matrix[...][0]
+
         int col0 = 1;
 
-        // Step 1: Store row and column information
-        // in the first row and first column of the matrix
         for (int i = 0; i < n; i++) {
-
-            // Check first column separately
-            if (matrix[i][0] == 0)
-                col0 = 0;
-
-            for (int j = 1; j < m; j++) {
+            for (int j = 0; j < m; j++) {
 
                 if (matrix[i][j] == 0) {
 
-                    // Mark this row
+                    // mark the row
                     matrix[i][0] = 0;
 
-                    // Mark this column
-                    matrix[0][j] = 0;
+                    // mark the col
+                    if (j != 0) {
+                        matrix[0][j] = 0;
+                    }
+                    else {
+                        col0 = 0;
+                    }
                 }
             }
         }
 
-        // Step 2: Use the markers to set cells to zero
-        // Start from row 1 and column 1
         for (int i = 1; i < n; i++) {
-
             for (int j = 1; j < m; j++) {
 
-                // If row i OR column j was marked
+                // check for col & row
                 if (matrix[i][0] == 0 || matrix[0][j] == 0) {
                     matrix[i][j] = 0;
                 }
             }
         }
 
-        // Step 3: Handle FIRST ROW
+        // step 3: finally mark the 1st row
         if (matrix[0][0] == 0) {
-
             for (int j = 0; j < m; j++) {
                 matrix[0][j] = 0;
             }
         }
 
-        // Step 4: Handle FIRST COLUMN
+        // step 4: finally mark the 1st col
         if (col0 == 0) {
-
             for (int i = 0; i < n; i++) {
                 matrix[i][0] = 0;
             }
