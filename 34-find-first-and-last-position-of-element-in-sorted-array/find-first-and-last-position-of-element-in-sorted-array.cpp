8efc6faaf5a214c245572dece8f7d445bb9240b2
vakshat10 <vakshatsw@gmail.com>
2026-08-15 23:11:39 +0530

    Time: 0 ms (100.00%) | Memory: 17.7 MB (19.74%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,18 +1,19 @@
 class Solution {
 public:
     vector<int> searchRange(vector<int>& nums, int target) {
-        int n = nums.size();
-        int lb = lower_bound(nums.begin(),nums.end(),target)-nums.begin();
-        int ub = upper_bound(nums.begin(),nums.end(),target)-nums.begin();
-        
-        if(ub-lb == 0){   // target does not exist
-            return {-1,-1};
+        int lb = lower_bound(nums.begin(), nums.end(), target) - nums.begin();
+        int ub = upper_bound(nums.begin(), nums.end(), target) - nums.begin();
+
+        // Target does not exist
+        if(lb == ub) {
+            return {-1, -1};
         }
-        else if(ub == lb+1){  // target appears single time
-            return {lb,lb};
-        }
-        else{                   // target appears multiple time
-            return {lb,ub-1};
+
+        // First occurrence = lb
+        // Last occurrence = ub - 1
+
+        else{
+             return {lb, ub - 1};
         }
     }
 };
