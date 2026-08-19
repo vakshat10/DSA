720c5b526679ba015f4757b9644bdbde1eba2800
vakshat10 <vakshatsw@gmail.com>
2026-08-19 14:10:48 +0530

    Time: 0 ms (100.00%) | Memory: 26.2 MB (69.87%) - LeetSync

--- 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.cpp
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.cpp
@@ -2,47 +2,54 @@ class Solution {
 public:
     int singleNonDuplicate(vector<int>& nums) {
         int n = nums.size();
+
         int low = 1;
-        int high = n-2;
-        if(n == 1){
-          return nums[0];
-        }
-        if(nums[0]!=nums[1]){
+        int high = n - 2;
+
+        if (n == 1) {
             return nums[0];
         }
-        if (nums[n-1]!=nums[n-2]){
-            return nums[n-1];
+
+        if (nums[0] != nums[1]) {
+            return nums[0];
         }
-        
-        while(low<=high){
-            int mid = low +(high-low)/2;
-            
-            
-            if(nums[mid] == nums[mid-1]){
-                if(mid%2 == 0){
-                    high = mid-1;
-                }
-                else{
-                    low = mid+1;
-                }
 
+        if (nums[n - 1] != nums[n - 2]) {
+            return nums[n - 1];
+        }
 
-            }
-            else if (nums[mid] == nums[mid+1]){
-                if(mid%2 == 0){
-                    low =mid+1;
-                }
-                else{
-                    high = mid-1;
-                }
+        while (low <= high) {
 
-            }
-            else{
+            int mid = low + (high - low) / 2;
+
+            // mid itself is the single element
+            if (nums[mid] != nums[mid - 1] &&nums[mid] != nums[mid + 1]) {
                 return nums[mid];
             }
-        
+
+            // mid is paired with left
+            else if (nums[mid] == nums[mid - 1]) {
+
+                if (mid % 2 == 0) {
+                    high = mid - 1;
+                }
+                else {
+                    low = mid + 1;
+                }
+            }
+
+            // mid is paired with right
+            else if (nums[mid] == nums[mid + 1]) {
+
+                if (mid % 2 == 0) {
+                    low = mid + 1;
+                }
+                else {
+                    high = mid - 1;
+                }
+            }
         }
-        return 0;
-        
+
+        return -1;
     }
 };
