7b21910acc72fb532a406ab76417e4a892c0a5dd
vakshat10 <vakshatsw@gmail.com>
2026-08-19 16:55:13 +0530

    Time: 0 ms (100.00%) | Memory: 26.1 MB (69.87%) - LeetSync

--- 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.cpp
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.cpp
@@ -6,30 +6,36 @@ public:
         int low = 1;
         int high = n - 2;
 
+        // If only one element is present
         if (n == 1) {
             return nums[0];
         }
 
+        // Check if first element is single
         if (nums[0] != nums[1]) {
             return nums[0];
         }
 
+        // Check if last element is single
         if (nums[n - 1] != nums[n - 2]) {
             return nums[n - 1];
         }
 
+        // Binary search
         while (low <= high) {
 
             int mid = low + (high - low) / 2;
 
             // mid itself is the single element
-            if (nums[mid] != nums[mid - 1] &&nums[mid] != nums[mid + 1]) {
+            if (nums[mid] != nums[mid - 1] &&
+                nums[mid] != nums[mid + 1]) {
                 return nums[mid];
             }
 
-            // mid is paired with left
+            // mid is paired with the left element
             else if (nums[mid] == nums[mid - 1]) {
 
+                // Pair should start at even index
                 if (mid % 2 == 0) {
                     high = mid - 1;
                 }
@@ -38,9 +44,10 @@ public:
                 }
             }
 
-            // mid is paired with right
+            // mid is paired with the right element
             else if (nums[mid] == nums[mid + 1]) {
 
+                // Pair should start at even index
                 if (mid % 2 == 0) {
                     low = mid + 1;
                 }
