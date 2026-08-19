28b8883e9fea61458842d5273d04cfc90ed75705
vakshat10 <vakshatsw@gmail.com>
2026-08-19 17:28:18 +0530

    Time: 0 ms (100.00%) | Memory: 12.7 MB (43.86%) - LeetSync

--- 162-find-peak-element/find-peak-element.cpp
+++ 162-find-peak-element/find-peak-element.cpp
@@ -3,34 +3,41 @@ public:
     int findPeakElement(vector<int>& nums) {
         int n = nums.size();
 
-        if(n==1) return 0;
+        // If only one element, it is the peak
+        if (n == 1) return 0;
 
-        if(nums[0]>nums[1]) return 0;
-        
-        if(nums[n-1]>nums[n-2])  return n-1;
+        // Check first element
+        if (nums[0] > nums[1]) return 0;
+
+        // Check last element
+        if (nums[n - 1] > nums[n - 2]) return n - 1;
 
         int low = 1;
-        int high = n-2;
+        int high = n - 2;
 
-        while(low<=high){
-            int mid = (low+high)/2;
+        while (low <= high) {
 
-            if(nums[mid]>nums[mid+1] && nums[mid]>nums[mid-1]){
+            int mid = low + (high - low) / 2;
+
+            // If mid is greater than both neighbours,
+            // then mid is a peak
+            if (nums[mid] > nums[mid + 1] && nums[mid] > nums[mid - 1]) {
                 return mid;
-
-            } 
-
-            else if(nums[mid]<nums[mid+1]){
-                low = mid +1;
             }
+
+            // We are going uphill,
+            // so a peak is guaranteed on the right
+            else if (nums[mid] < nums[mid + 1]) {
+                low = mid + 1;
+            }
+
+            // We are going downhill,
+            // so a peak is guaranteed on the left
             else {
-                high = mid-1;
+                high = mid - 1;
             }
-
         }
 
         return -1;
-
-        
     }
 };
