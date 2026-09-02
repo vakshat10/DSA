a1efbf3a8020c151c8f206f0873834db32f42c3a
vakshat10 <vakshatsw@gmail.com>
2026-09-02 23:13:25 +0530

    Time: 7 ms (8.52%) | Memory: 17.8 MB (66.30%) - LeetSync

--- 2032-largest-odd-number-in-string/largest-odd-number-in-string.cpp
+++ 2032-largest-odd-number-in-string/largest-odd-number-in-string.cpp
@@ -4,12 +4,18 @@ public:
 
         int n = num.size();
 
+        // Find the rightmost odd digit
         for(int i = n - 1; i >= 0; i--) {
-            if((num[i] ) % 2 == 1) {
+
+            // Convert character digit to integer and check if odd
+            if((num[i] - '0') % 2 == 1) {
+
+                // Take the string from index 0 to i
                 return num.substr(0, i + 1);
             }
         }
 
+        // No odd digit found
         return "";
     }
 };
