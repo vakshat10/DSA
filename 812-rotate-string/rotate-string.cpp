faa27d81c5a18afbe30459fd5a058dd16e744753
vakshat10 <vakshatsw@gmail.com>
2026-09-04 00:29:01 +0530

    Time: 0 ms (100.00%) | Memory: 8.2 MB (6.10%) - LeetSync

--- 812-rotate-string/rotate-string.cpp
+++ 812-rotate-string/rotate-string.cpp
@@ -2,32 +2,14 @@ class Solution {
 public:
     bool rotateString(string s, string goal) {
         int n = s.size();
+        int m = goal.size();
 
         // Both strings must have the same length
-        if(s.size() != goal.size())
-            return false;
+        if(n != m) return false;
 
-        // Try all possible rotations
-        for(int j = 1; j <= n; j++) {
-
-            // Create string of size n to store the rotated string
-            string ans(n, ' ');
-
-            // Move the first character to the end
-            ans[n - 1] = s[0];
-
-            // Shift all remaining characters one position to the left
-            for(int i = 0; i < n - 1; i++) {
-                ans[i] = s[i + 1];
-            }
-
-            // Update s with the rotated string
-            s = ans;
-
-            // Check if current rotation matches goal
-            if(ans == goal) {
-                return true;
-            }
+        // Every rotation of s will appear in s + s
+        if((s + s).find(goal) != string::npos){
+            return true;
         }
 
         return false;
