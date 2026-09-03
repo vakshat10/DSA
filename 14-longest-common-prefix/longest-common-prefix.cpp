08f03aca5b184bee9f1397cb66a51019aceaa097
vakshat10 <vakshatsw@gmail.com>
2026-09-03 17:26:58 +0530

    Time: 0 ms (100.00%) | Memory: 11.9 MB (58.68%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.cpp
+++ 14-longest-common-prefix/longest-common-prefix.cpp
@@ -3,32 +3,38 @@ public:
     string longestCommonPrefix(vector<string>& strs) {
         int n = strs.size();
         string ans = "";
-        if(n==0){
-            return ans;
-        }
-        if(n==1){
-            return strs[0];
-        }
-        int num = INT_MAX; // stores no of same elements
-        for(int i = 0;i<n-1;i++){
-            int count = 0;
-            for(int j = 0;j<strs[i].size() && j<strs[i+1].size();j++){
-                
-                if(strs[i][j]==strs[i+1][j]){
-                    count++;
-                }
-                else{
-                    break;
-                }
-                
-            }
-            num = min(num,count);
 
+        // Handle empty vector
+        if(n == 0)
+            return ans;
+
+        // If only one string, it is the prefix
+        if(n == 1)
+            return strs[0];
+
+        int num = INT_MAX; // Minimum common prefix length
+
+        // Compare every pair of adjacent strings
+        for(int i = 0; i < n - 1; i++) {
+            int count = 0;
+
+            // Compare characters at the same index
+            for(int j = 0; j < strs[i].size() && j < strs[i + 1].size(); j++) {
+                if(strs[i][j] == strs[i + 1][j])
+                    count++;
+                else
+                    break;
+            }
+
+            // Keep the smallest common prefix length
+            num = min(num, count);
         }
-        for(int i = 0;i<num;i++){
+
+        // Build the final answer using the first string
+        for(int i = 0; i < num; i++) {
             ans += strs[0][i];
         }
+
         return ans;
-        
     }
 };
